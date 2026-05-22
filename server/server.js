76a41cb0a21cebad4ea88a385fa696286758ffa0
HarshitRaj2712 <panditjee2712@gmail.com>
2026-05-22 22:52:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5001;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server executing in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    console.log(`Server executingin ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
   });
 }).catch(err => {
   console.error('Failed to initialize database connection. Server starting anyway for offline client support.', err);
